f714b5453ef039f9b2ecbef274930fa4fafc233c
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-09-24 18:11:08 +0600

    Update utils.py

--- neuroclassify/utils.py
+++ neuroclassify/utils.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 
 def load_and_preprocess_image(img_path, target_size=(150, 150)):
@@ -16,24 +15,16 @@ def load_and_preprocess_image(img_path, target_size=(150, 150)):
         return None
 
 def predict_image(model, img_path, class_labels, img_size=(150, 150)):
-    """Predict the class of an image and display the result."""
+    """Predict the class of an image."""
     img_array = load_and_preprocess_image(img_path, target_size=img_size)
     if img_array is None:
-        return None, None  # Exit if image loading failed
+        return None  # Exit if image loading failed
 
     predictions = model.predict(img_array)  # Get predictions
     predicted_class = np.argmax(predictions, axis=1)[0]
     predicted_class_name = class_labels[predicted_class]
-    predicted_probabilities = predictions[0]  # Get probabilities for all classes
 
-    # Display the image
-    img = image.load_img(img_path)
-    plt.imshow(img)
-    plt.title(f'Predicted: {predicted_class_name}\nProbabilities: {predicted_probabilities}')
-    plt.axis('off')
-    plt.show()
-
-    return predicted_class_name, predicted_probabilities
+    return predicted_class_name  # Return only the class name
 
 def predict_images(model, img_dir, class_labels, img_size=(150, 150)):
     """Predict classes for all images in a directory."""
@@ -43,7 +34,7 @@ def predict_images(model, img_dir, class_labels, img_size=(150, 150)):
     for filename in os.listdir(img_dir):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
             img_path = os.path.join(img_dir, filename)
-            predicted_class_name, predicted_probabilities = predict_image(model, img_path, class_labels, img_size)
-            predictions.append((filename, predicted_class_name, predicted_probabilities))
+            predicted_class_name = predict_image(model, img_path, class_labels, img_size)
+            predictions.append((filename, predicted_class_name))
     
-    return predictions
+    return predictions  # Return class names only
