12a9dac9d7d47599b888f2a219a3237df44c6743
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-11-23 17:13:01 +0600

    Update predict.py

--- neuroclassify/predict.py
+++ neuroclassify/predict.py
@@ -1,9 +1,11 @@
+import os
+import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from .classifier import ImageClassifier
 
-def predict_image(model, img_path, label_file):
-    """Predict the class of a single image."""
+def predict_image(model, img_path, label_file, display=False):
+    """Predict the class of a single image and optionally display the image."""
     # Load image
     img = image.load_img(img_path, target_size=(150, 150))
     img_array = image.img_to_array(img)
@@ -17,14 +19,21 @@ def predict_image(model, img_path, label_file):
     predicted_class = np.argmax(predictions, axis=1)[0]
     predicted_class_name = list(class_indices.keys())[predicted_class]
 
+    if display:
+        # Display the image with predicted class
+        plt.imshow(img)
+        plt.title(f'Predicted Class: {predicted_class_name}')
+        plt.axis('off')  # Hide axes for a cleaner image display
+        plt.show()
+
     return predicted_class_name
 
-def predict_images(model, img_dir, label_file):
-    """Predict the class of all images in a directory."""
+def predict_images(model, img_dir, label_file, display=False):
+    """Predict the class of all images in a directory and optionally display them."""
     predictions = []
     for filename in os.listdir(img_dir):
         img_path = os.path.join(img_dir, filename)
-        predicted_class_name = predict_image(model, img_path, label_file)
+        predicted_class_name = predict_image(model, img_path, label_file, display)
         predictions.append((filename, predicted_class_name))
 
     return predictions
