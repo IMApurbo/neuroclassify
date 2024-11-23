d5ee5c7386c16a1f1d8227daaec12f6bf60ff9ad
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-11-23 17:16:49 +0600

    Update __init__.py

--- neuroclassify/__init__.py
+++ neuroclassify/__init__.py
@@ -1,2 +1,11 @@
+# neuroclassify/__init__.py
+
+# Import the main components of the package for easier access
+
 from .classifier import ImageClassifier
-from .utils import load_and_preprocess_image, predict_image, predict_images
+from .predict import predict_image, predict_images
+from .utils import save_labels, load_labels
+
+
+# __all__ to specify what gets imported when using 'from neuroclassify import *'
+__all__ = ['ImageClassifier', 'predict_image', 'predict_images', 'save_labels', 'load_labels']
